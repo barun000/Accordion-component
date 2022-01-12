722fb4c90f366bc21a52a2c217ca07ea58279cef
barun000 <barunmishra041@gmail.com>
2022-01-12 17:56:37 +0530

    Done

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -4,12 +4,14 @@ import Accordion from './Accordion';
 
 export default function App() {
   const [active,setActive]=useState("");
+
   return (
     <div className="App">
      <Accordion title="Title 1" active={active} setActive={setActive}/>
      <Accordion title="Title 2" active={active} setActive={setActive}/>
      <Accordion title="Title 3" active={active} setActive={setActive}/>
      <Accordion title="Title 4" active={active} setActive={setActive}/>
+     <Accordion title="Title 5" active={active} setActive={setActive}/>
     </div>
   );
 }
